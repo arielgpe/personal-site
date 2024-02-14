e4839721ee56fa6a8791b77d59e41940352e8c08
Ariel Guzman <ariel.guzman01@gmail.com>
2024-02-14 16:38:20 -0400

    strapi url

--- src/utils/getStrapiClient.ts
+++ src/utils/getStrapiClient.ts
@@ -2,9 +2,10 @@ import Strapi, { StrapiOptions } from 'strapi-sdk-js';
 
 
 const STRAPI_TOKEN = process.env.STRAPI_TOKEN || '';
+const STRAPI_URL = process.env.STRAPI_URL || '';
 
 export const getStrapiClient = (options?: StrapiOptions) => {
-  const strapi = new Strapi(options);
+  const strapi = new Strapi({url: STRAPI_URL ,...options});
   strapi.setToken(STRAPI_TOKEN);
 
   return strapi;
