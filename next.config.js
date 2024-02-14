a76f8c0a483b38e1269342eac13c84ab847d18be
Ariel Guzman <ariel.guzman01@gmail.com>
2024-02-14 16:33:34 -0400

    variable and favicon

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     },
     env: {
         STRAPI_TOKEN: process.env.STRAPI_TOKEN || '',
+        STRAPI_URL: process.env.STRAPI_URL || 'http://localhost:1337',
         GA_MEASUREMENT_ID: process.env.GA_MEASUREMENT_ID || '',
     }
 };
